1b501f48c7cefbe702990e285c4b2de6bf5c20da
alanmdsan <alanmdsan@gmail.com>
2024-01-11 15:49:31 -0300

    testing comments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,32 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+    test('Deve renderizar o componente corretamente', () => {
+        render(<PostComment/>)
+        expect(screen.getByText('Comentar')).toBeInTheDocument()
+    })
+    
+    test('Deve verificar que dois comentários foram inseridos e exibidos corretamente', () => {
+        render(<PostComment/>)
+        
+        const botao = screen.getByTestId('button-comentar')
+        const textarea = screen.getByTestId('textarea-comentar')
+        
+        fireEvent.change(textarea, {
+            target: {
+                value: 'comentário 1'
+            }
+        })
+        fireEvent.click(botao)
+
+        fireEvent.change(textarea, {
+            target: {
+                value: 'comentário 2'
+            }
+        })
+        fireEvent.click(botao)
+
+        expect(screen.getByText('comentário 1')).toBeInTheDocument();
+        expect(screen.getByText('comentário 2')).toBeInTheDocument();
     });
 });
